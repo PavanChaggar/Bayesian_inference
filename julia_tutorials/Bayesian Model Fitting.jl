e75aa0888592cd77f0cefdb90bba5b34c9cf258e
Pavan Chaggar <pavanjit.chaggar@exeter.ox.ac.uk>
2020-07-26 19:16:31 +0100

    adding likelihood function and plotting

--- julia_tutorials/Bayesian Model Fitting.jl
+++ julia_tutorials/Bayesian Model Fitting.jl
@@ -3,11 +3,27 @@
 using Plots
 using Distributions
 
-n = 10
-μ = 5.0 * ones(n)
-σ = 4.0 * ones(n)
-ran = rand(Normal(1,1),n)
 
-y = μ .+ (σ .* ran)
+μ = 7.0
+σ = 2.0
+n = 1000
+lim = 10
+x = range(0, lim, length = n)
 
-plot(y)
+f(μ, σ, x) = μ .+ (σ * rand(Normal(0,1),lim))
+
+y = f(μ, σ, x)
+
+function likelihood!(y, n, sd)
+    yy = repeat(y, outer=(1,n))
+    ran = range(0, stop = length(y), length = n)
+    xx = repeat(ran, outer=(1, length(y)))'
+    likelihood = exp.(-0.5 * sum((yy .- xx ).^2,dims=1) / σ^2)
+    norm_likelihood = likelihood / sum(likelihood)
+    return likelihood
+end
+
+likelihood = likelihood!(y, n, σ)
+
+plot(x,likelihood[1,:])
+scatter!(y)
